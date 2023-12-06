f501061ee9fa6743903b471361794389a749d310
WhizzyDoc <hassanridwan2536@gmail.com>
2023-12-06 08:08:32 +0100

    first commit

---
+++ static/js/functions.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    
+})
+
+/* Navigation bar */
+/* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
+function openNav() {
+    $(".sidenav").toggleClass('active');
+    $("main").toggleClass('active');
+  }
